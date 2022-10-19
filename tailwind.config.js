ab44c0f9ff88049f0c9e62fa98ac145125c5de11
don-Carlos99 <carlos.r@eztechmedia.com>
2022-10-19 19:43:43 +0500

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans : ['Rubik', 'sans-serif'],
+      },
+      spacing : {
+        "18" : "4.5rem",
+      }
+    },
+  },
+  plugins: [],
+}
